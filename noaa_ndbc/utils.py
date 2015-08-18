bc523e7dc8eefbf5134b8256729752d01c17c560
mortenwh <morten.wergeland.hansen@gmail.com>
2015-08-18 15:09:18 +0200

    added app for noaa-ndbc buoys - probably to be changed

---
+++ noaa_ndbc/utils.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:		utils.py
+# Purpose:      
+#
+# Author:       Morten Wergeland Hansen
+# Modified:	Morten Wergeland Hansen
+#
+# Created:	21.01.2015
+# Last modified:24.01.2015 21:57
+# Copyright:    (c) NERSC
+# License:      
+#-------------------------------------------------------------------------------
+
+def populate_stdmbuoys():
+
