8cad6c50fb4157942ac3e3c1bbd1c6afc4dc2816
mortenwh <morten.wergeland.hansen@gmail.com>
2015-09-17 17:30:21 +0200

    added test config and setup.py - nansencloud can now be tested with 'python setup.py test'

---
+++ setup.py
@@ -0,0 +1,47 @@
+#-------------------------------------------------------------------------------
+# Name:
+# Purpose:      
+#
+# Author:       Morten Wergeland Hansen
+# Modified:
+#
+# Created:
+# Last modified:
+# Copyright:    (c) NERSC
+# License:      
+#-------------------------------------------------------------------------------
+from setuptools import setup, find_packages
+import sys
+
+readme_file = 'README.md'
+try:
+    long_description = open(readme_file).read()
+except IOError:
+    sys.stderr.write("[ERROR] Cannot find file specified as "
+        "``long_description`` (%s)\n" % readme_file)
+    sys.exit(1)
+
+install_requires = []
+
+setup(name='nansen-cloud',
+      version=0.1,
+      description='Nansen Center geospatial data management application',
+      long_description=long_description,
+      zip_safe=False,
+      author=('Morten W. Hansen', 'Anton Korosov'),
+      author_email='mortenh@nersc.no',
+      url='#',
+      download_url='#',
+      packages = find_packages(),
+      include_package_data=True,
+      install_requires = install_requires,
+      test_suite='test_nansencloud.runtests.runtests',
+      classifiers = ['Development Status :: 0 - Beta',
+                     'Environment :: Web Environment',
+                     'Framework :: Django',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: BSD License',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python',
+                     'Topic :: Utilities'],
+      )
