d01e653eae9d911ab72f4374c618072f822702cb
mortenwh <morten.wergeland.hansen@gmail.com>
2015-09-25 15:49:38 +0200

    noaa management command should work with current noaa_ndbc model

--- nansencloud/noaa_ndbc/management/commands/crawl_ndbc_stdmet.py
+++ nansencloud/noaa_ndbc/management/commands/crawl_ndbc_stdmet.py
@@ -11,12 +11,11 @@
 # License:      
 #-------------------------------------------------------------------------------
 from django.core.management.base import BaseCommand, CommandError
+from django.contrib.gis.geos import GEOSGeometry
 
 import netCDF4
 from thredds_crawler.crawl import Crawl
-from noaa_ndbc.models import StandardMeteorologicalBuoy
-
-url='http://dods.ndbc.noaa.gov/thredds/catalog/data/stdmet/catalog.xml'
+from nansencloud.noaa_ndbc.models import StandardMeteorologicalBuoy
 
 class Command(BaseCommand):
     args = '<year>'
@@ -25,17 +24,19 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if len(args)==0:
             raise IOError('Please provide a year')
+        url='http://dods.ndbc.noaa.gov/thredds/catalog/data/stdmet/catalog.xml'
         year = int(args[0])
         # Create regex string
         regex = '(.*%s\.nc)' %year
         c = Crawl(url, select=[regex])
+        added = 0
         for ds in c.datasets:
-            url = [s.get('url') for s in d.services if
+            url = [s.get('url') for s in ds.services if
                     s.get('service').lower()=='opendap'][0]
             nc_dataset = netCDF4.Dataset(url)
             station = nc_dataset.station
-            longitude = dataset.variables['longitude']
-            latitude = dataset.variables['latitude']
+            longitude = nc_dataset.variables['longitude']
+            latitude = nc_dataset.variables['latitude']
             location = GEOSGeometry('POINT(%s %s)' %(longitude[0],latitude[0]))
             ndbc_stdmet = StandardMeteorologicalBuoy(
                     station = station,
