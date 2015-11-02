e73790cb0ab9e256ccd5d6fc1edb906fabb2eb81
akorosov <korosov@gmail.com>
2015-11-02 14:51:42 +0100

    add admin to catalog

--- nansencloud/catalog/models.py
+++ nansencloud/catalog/models.py
@@ -23,6 +23,9 @@ class Source(models.Model):
     specs = models.CharField(max_length=50, default='',
         help_text='Further specifications of the source.')
 
+    class Meta:
+        unique_together = (("platform", "instrument", "specs"),)
+
     def __str__(self):
         return '%s/%s' % (self.platform, self.instrument)
 
