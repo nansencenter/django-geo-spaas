837f243304b66f51d725e81e9481409cc8e69990
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2015-12-10 10:55:47 +0100

    made functions in viewer.models.Dataset to return full javascript function - later extend to return geometry in line with the dataset geometry (not necessarily polygon)

--- nansencloud/viewer/models.py
+++ nansencloud/viewer/models.py
@@ -29,6 +29,13 @@ class Dataset(CatalogDataset):
     class Meta:
         proxy = True
 
+    # TODO: return javascript string according to geometry type
+    def geo_js(self):
+        return "L.polygon( %s, {color: '#b20000', weight: 1, fillOpacity: 0.2, fillColor: '#f00'});" %self.border2str()
+
+    def const_geo_js(self):
+        return "L.polygon( %s, {color: '#b20000', weight: 0.5, fillOpacity: 0.05, fillColor: '#b20000'});" %self.border2str()
+
     def border2str(self):
         ''' Generate Leaflet JavaScript defining the border polygon'''
         borderStr = '['
