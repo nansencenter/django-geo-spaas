228896db7105bd5af21dc0273a29278e9096d349
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2015-12-25 13:57:13 +0100

    ingestion of sar quad pol is working. Code should be moved into sar_processing app

--- nansencloud/ingestor/management/commands/ingest_sar_quad_pol.py
+++ nansencloud/ingestor/management/commands/ingest_sar_quad_pol.py
@@ -1,4 +1,5 @@
 import os, glob, warnings
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
 from sarqp.sarqp import QuadPol
@@ -14,21 +15,40 @@ class Command(BaseCommand):
 
         non_ingested_uris = DataLocation.objects.all().get_non_ingested_uris(args)
         for non_ingested_uri in non_ingested_uris:
+            model_wind_path = os.path.join(settings.DOWNLOAD_ROOT, 'ncep')
             wind_product_path = os.path.join(settings.PRODUCT_ROOT, 'sar_wind')
             wind_product_basename = os.path.basename(non_ingested_uri
                         ).split('.')[0] + '_windfield.nc'
             qp = QuadPol(non_ingested_uri, wind_direction='ncep_wind_online',
                     wind_product_path = wind_product_path,
-                    wind_product_basename = wind_product_basename)
+                    wind_product_basename = wind_product_basename,
+                    model_wind_path=model_wind_path)
 
-            ncFile = os.path.join(settings.PRODUCT_ROOT, 'sar_quad_pol',
-                os.path.basename(uri).split('.')[0] + '.nc')
+            # Store processed SAR netCDF
+            product_dir_name = 'sar_quad_pol'
+            if not os.path.exists(os.path.join(settings.PRODUCT_ROOT,
+                    product_dir_name)):
+                os.mkdir(os.path.join(settings.PRODUCT_ROOT, product_dir_name))
+            ncFile = os.path.join(settings.PRODUCT_ROOT, product_dir_name,
+                os.path.basename(non_ingested_uri).split('.')[0] + '.nc')
             qp.export(ncFile)
+
+            # Ingest model wind field
+            model_wind_file = qp.get_metadata(
+                    bandID='windspeed')['model_wind_file']
+            self.stdout.write('Ingesting %s ...\n' % model_wind_file)
+            ds, cr = Dataset.objects.get_or_create(model_wind_file)
+            self.stdout.write('Successfully added: %s\n' % model_wind_file)
+
+            # Ingest processed SAR wind netCDF
+            wind_nc_file = os.path.join(wind_product_path, wind_product_basename)
+            self.stdout.write('Ingesting %s ...\n' % wind_nc_file)
+            ds, cr = Dataset.objects.get_or_create(wind_nc_file)
+            self.stdout.write('Successfully added: %s\n' %wind_nc_file )
+
+            # Ingest processed SAR netCDF
             self.stdout.write('Ingesting %s ...\n' % ncFile)
             ds, cr = Dataset.objects.get_or_create(ncFile)
             self.stdout.write('Successfully added: %s\n' % ncFile)
 
-            ds, cr = Dataset.objects.get_or_create(os.path.join(
-                wind_product_path, wind_product_basename))
-            self.stdout.write('Successfully added: %s\n' %os.path.join(
-                wind_product_path, wind_product_basename) )
+
