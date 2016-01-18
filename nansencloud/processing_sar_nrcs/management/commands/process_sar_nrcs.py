54e8e253d73f5819a7385276746b11ff1023e11e
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2016-01-18 11:56:20 +0100

    made in viewer.tools to return media path

--- nansencloud/processing_sar_nrcs/management/commands/process_sar_nrcs.py
+++ nansencloud/processing_sar_nrcs/management/commands/process_sar_nrcs.py
@@ -8,6 +8,8 @@ from django.core.management.base import BaseCommand
 from nansencloud.catalog.models import Dataset, Product
 from nansencloud.catalog.models import DataLocation
 
+from nansencloud.viewer import tools as vtools
+
 from nansencloud.processing_sar_nrcs.tools import *
 
 from nansat.nansat import Domain, Nansat, Figure, NSR
@@ -87,27 +89,11 @@ class Command(BaseCommand):
             for i, band in enumerate(s0bands):
                 s0bands[i] = band+'_reduced'
 
-        # Get the path of nansencloud media files
-        ncloud_media_path = os.path.join(settings.MEDIA_ROOT,
-                self.__module__.split('.')[0])
-        if not os.path.exists(ncloud_media_path):
-            os.mkdir(ncloud_media_path)
-
-        # Get the path of nansencloud.processing_sar_nrcs media files
-        proc_nrcs_media_path = os.path.join(ncloud_media_path,
-                self.__module__.split('.')[1])
-        if not os.path.exists(proc_nrcs_media_path):
-            os.mkdir(proc_nrcs_media_path)
-
-        # Get the path of products of the provided dataset
-        prodBaseName = os.path.split(n.fileName)[-1].split('.')[0]
-        products_media_path = os.path.join(proc_nrcs_media_path,
-                prodBaseName)
-        if not os.path.exists(products_media_path):
-            os.mkdir(products_media_path)
+        media_path = vtools.media_path(self.__module__,
+                os.path.split(n.fileName)[-1].split('.')[0])
 
         if qp:
-            n.export(os.path.join(products_media_path, prodBaseName+'.nc'))
+            n.export(os.path.join(media_path, prodBaseName+'.nc'))
 
         lon, lat = n.get_corners()
         d = Domain(NSR(3857),
@@ -132,41 +118,41 @@ class Command(BaseCommand):
             mask[s0 <= 0] = 0
             s0 = np.log10(s0)*10.
 
-            nansatFigure(s0, mask, min, max, products_media_path,
+            nansatFigure(s0, mask, min, max, media_path,
                     product_filename)
 
-            prodFileURI = os.path.join(products_media_path, product_filename)
+            prodFileURI = os.path.join(media_path, product_filename)
             prodHttpURI = os.path.join(httpURIbase, product_filename)
             create_product(prodHttpURI, ds, meta, 'dB')
             num_products += 1
 
         if qp:
             print('Make PR image...')
-            pname = make_PR_image(n, dir=products_media_path)
-            prodFileURI = os.path.join(products_media_path, pname)
+            pname = make_PR_image(n, dir=media_path)
+            prodFileURI = os.path.join(media_path, pname)
             prodHttpURI = os.path.join(httpURIbase, pname)
             create_product(prodHttpURI, ds, n.bands()['PR'], 'm/m')
             num_products += 1
 
             print('Make PD image...')
-            pname = make_PD_image(n, dir=products_media_path)
-            prodFileURI = os.path.join(products_media_path, pname)
+            pname = make_PD_image(n, dir=media_path)
+            prodFileURI = os.path.join(media_path, pname)
             prodHttpURI = os.path.join(httpURIbase, pname)
             create_product(prodHttpURI, ds, n.bands()['PD'], 'm/m')
             num_products += 1
 
             try:
                 print('Make NP image...')
-                pname = make_NP_image(n, dir=products_media_path)
-                prodFileURI = os.path.join(products_media_path, pname)
+                pname = make_NP_image(n, dir=media_path)
+                prodFileURI = os.path.join(media_path, pname)
                 prodHttpURI = os.path.join(httpURIbase, pname)
                 create_product(prodHttpURI, ds, n.bands()['PD'], 'm/m')
                 num_products += 1
             except Exception as e:
                 print e
             print('Make CP image...')
-            pname = make_NRCS_image(n, 'CP', dir=products_media_path)
-            prodFileURI = os.path.join(products_media_path, pname)
+            pname = make_NRCS_image(n, 'CP', dir=media_path)
+            prodFileURI = os.path.join(media_path, pname)
             prodHttpURI = os.path.join(httpURIbase, pname)
             create_product(prodHttpURI, ds, n.bands()['PD'], 'm/m')
             num_products += 1
--- nansencloud/viewer/tests.py
+++ nansencloud/viewer/tests.py
@@ -1,1 +1,17 @@
+import os
+from django.conf import settings
+from django.test import TestCase
 
+from nansencloud.viewer import tools as vtools
+
+class TestViewer(TestCase):
+
+    def test_media_path(self):
+
+        f = '/mnt/10.11.12.232/sat_downloads_asar/level-0/gsar_rvl/' \
+                'RVL_ASA_WS_20091116195940116.gsar'
+        media_path = vtools.media_path('nansencloud.processing_sar_doppler', f)
+        self.assertTrue(os.path.exists(media_path))
+        self.assertEqual(media_path, os.path.join(settings.MEDIA_ROOT,
+            'nansencloud', 'processing_sar_doppler',
+            'RVL_ASA_WS_20091116195940116'))
