b9a5a59a1d3923c415e7c0469c571da7d76e3dd8
mortenwh <morten.wergeland.hansen@gmail.com>
2016-02-24 17:45:46 +0100

    working on catalog tests

--- nansencloud/catalog/tests.py
+++ nansencloud/catalog/tests.py
@@ -1,15 +1,75 @@
-import datetime
+from django.utils import timezone
 
 from django.test import TestCase
 
 from django.contrib.gis.geos import Polygon
 
 from nansencloud.gcmd_keywords.models import Platform, Instrument
+from nansencloud.gcmd_keywords.models import ISOTopicCategory, DataCenter
 from nansencloud.catalog.models import *
 
-class DataCenterTests(TestCase):
+class DatasetTests(TestCase):
+    def test_dataset(self):
+        ''' Shall create Dataset instance '''
+        geolocation = GeographicLocation(
+                geometry=Polygon(((0, 0), (0, 10), (10, 10), (0, 10), (0, 0))))
+        geolocation.save()
+        iso_category = ISOTopicCategory.objects.get(name='Oceans')
+        dc = DataCenter.objects.get(short_name='NERSC')
+        p = Platform.objects.get(short_name='AQUA')
+        i = Instrument.objects.get(short_name='MODIS')
+        source = Source(platform=p, instrument=i)
+        source.save()
+        ds = Dataset(
+                entry_title='Test dataset', 
+                ISO_topic_category = iso_category,
+                data_center = dc,
+                summary = 'This is a quite short summary about the test' \
+                            ' dataset.',
+                time_coverage_start=timezone.datetime(2010,1,1,
+                    tzinfo=timezone.utc),
+                time_coverage_end=timezone.datetime(2010,1,2,
+                    tzinfo=timezone.utc),
+                source=source,
+                geographic_location=geolocation)
+        ds.save()
+        # Add parameter
 
-    pass
+class DatasetURITests(TestCase):
+
+    def test_DatasetURI(self):
+        uri = 'file://this/is/some/file'
+        mock_dataset = mock.create_autospec(Dataset)
+        dsuri = DatasetURI(uri=uri, dataset=mock_dataset)
+        dsuri.save()
+        etitle = dsuri.dataset.entry_title 
+        # Check that the entry_title attribute was accessed
+
+
+        ''' Shall create DataLocation instance '''
+        geolocation = GeographicLocation(
+                geometry=Polygon(((0, 0), (0, 10), (10, 10), (0, 10), (0, 0))))
+        geolocation.save()
+        p = Platform.objects.get(short_name='AQUA')
+        i = Instrument.objects.get(short_name='MODIS')
+        source = Source(platform=p, instrument=i)
+        source.save()
+        dataset = Dataset(
+                    entry_title = 'Test dataset',
+                    ISO_topic_category = iso_category,
+                    data_center = dc,
+                    summary = 'This is a quite short summary about the test' \
+                                ' dataset.',
+                    time_coverage_start=timezone.datetime(2010,1,1,tzinfo=timezone.utc),
+                    time_coverage_end=timezone.datetime(2010,1,2,
+                        tzinfo=timezone.utc),
+                    source=source,
+                    geolocation=geolocation)
+        dataset.save()
+        dl = DataLocation(protocol=DataLocation.LOCALFILE,
+                          uri='URL',
+                          dataset=dataset)
+        dl.save()
 
 class DatasetParameterTests(TestCase):
 
@@ -27,56 +87,20 @@ class DatasetRelationshipTests(TestCase):
         source.save()
         child = Dataset(geolocation=geolocation,
                             source=source,
-                            time_coverage_start=datetime.datetime(2010,1,1),
-                            time_coverage_end=datetime.datetime(2010,1,2))
+                            time_coverage_start=timezone.datetime(2010,1,1,
+                                tzinfo=timezone.utc),
+                            time_coverage_end=timezone.datetime(2010,1,2,
+                                tzinfo=timezone.utc))
         child.save()
         parent = Dataset(geolocation=geolocation,
                             source=source,
-                            time_coverage_start=datetime.datetime(2010,1,2),
-                            time_coverage_end=datetime.datetime(2010,1,3))
+                            time_coverage_start=timezone.datetime(2010,1,2,
+                                tzinfo=timezone.utc),
+                            time_coverage_end=timezone.datetime(2010,1,3,
+                                tzinfo=timezone.utc))
         parent.save()
         dr = DatasetRelationship(child=child, parent=parent)
         dr.save()
-
-class DatasetTests(TestCase):
-    def test_dataset(self):
-        ''' Shall create Dataset instance '''
-        geolocation = GeographicLocation(
-                geometry=Polygon(((0, 0), (0, 10), (10, 10), (0, 10), (0, 0))))
-        geolocation.save()
-        p = Platform.objects.get(short_name='AQUA')
-        i = Instrument.objects.get(short_name='MODIS')
-        source = Source(platform=p, instrument=i)
-        source.save()
-        ds = Dataset(entry_title='Test dataset', 
-                time_coverage_start=datetime.datetime(2010,1,1),
-                time_coverage_end=datetime.datetime(2010,1,2),
-                source=source,
-                geographic_location=geolocation)
-        ds.save()
-        # Add parameter
-
-class DatasetURITests(TestCase):
-    def test_DatasetURI(self):
-        ''' Shall create DataLocation instance '''
-        geolocation = GeographicLocation(
-                geometry=Polygon(((0, 0), (0, 10), (10, 10), (0, 10), (0, 0))))
-        geolocation.save()
-        p = Platform.objects.get(short_name='AQUA')
-        i = Instrument.objects.get(short_name='MODIS')
-        source = Source(platform=p, instrument=i)
-        source.save()
-        dataset = Dataset(
-                    entry_title = 'Test dataset',
-                    time_coverage_start=datetime.datetime(2010,1,1),
-                    time_coverage_end=datetime.datetime(2010,1,2),
-                    source=source,
-                    geolocation=geolocation)
-        dataset.save()
-        dl = DataLocation(protocol=DataLocation.LOCALFILE,
-                          uri='URL',
-                          dataset=dataset)
-        dl.save()
 
 class GeographicLocationTests(TestCase):
     def test_geographiclocation(self):
@@ -121,8 +145,10 @@ class ProductTests(TestCase):
         source.save()
         dataset = Dataset(geolocation=geolocation,
                             source=source,
-                            time_coverage_start=datetime.datetime(2010,1,1),
-                            time_coverage_end=datetime.datetime(2010,1,2))
+                            time_coverage_start=timezone.datetime(2010,1,1,
+                                tzinfo=timezone.utc),
+                            time_coverage_end=timezone.datetime(2010,1,2,
+                                tzinfo=timezone.utc))
         dataset.save()
         location = DataLocation(protocol=DataLocation.LOCALFILE,
                                 dataset=dataset,
@@ -133,7 +159,7 @@ class ProductTests(TestCase):
                         long_name='Temperature of Sea Surface',
                         units='K',
                         location=location,
-                        time=datetime.datetime(2010,1,2))
+                        time=timezone.datetime(2010,1,2, tzinfo=timezone.utc))
         var.save()
 
     def test_search_datasets(self):
@@ -149,8 +175,10 @@ class ProductTests(TestCase):
         geolocation1.save()
         dataset1 = Dataset(geolocation=geolocation1,
                             source=source,
-                            time_coverage_start=datetime.datetime(2010,1,1),
-                            time_coverage_end=datetime.datetime(2010,1,2))
+                            time_coverage_start=timezone.datetime(2010,1,1,
+                                tzinfo=timezone.utc),
+                            time_coverage_end=timezone.datetime(2010,1,2,
+                                tzinfo=timezone.utc))
         dataset1.save()
         location1 = DataLocation(protocol=DataLocation.LOCALFILE,
                                 dataset=dataset1,
@@ -161,7 +189,7 @@ class ProductTests(TestCase):
                         long_name='Temperature of Sea Surface',
                         units='K',
                         location=location1,
-                        time=datetime.datetime(2010,1,2))
+                        time=timezone.datetime(2010,1,2, tzinfo=timezone.utc))
         prod.save()
 
         geolocation2 = GeographicLocation(
@@ -169,8 +197,10 @@ class ProductTests(TestCase):
         geolocation2.save()
         dataset2 = Dataset(geolocation=geolocation1,
                             source=source,
-                            time_coverage_start=datetime.datetime(2015,1,1),
-                            time_coverage_end=datetime.datetime(2015,1,2))
+                            time_coverage_start=timezone.datetime(2015,1,1,
+                                tzinfo=timezone.utc),
+                            time_coverage_end=timezone.datetime(2015,1,2,
+                                tzinfo=timezone.utc))
         dataset2.save()
         location2 = DataLocation(protocol=DataLocation.LOCALFILE,
                                 dataset=dataset2,
