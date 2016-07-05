ad71b745ea9a8eca2d2a53ad513e50788019260d
mortenwh <morten.wergeland.hansen@gmail.com>
2016-07-05 14:54:47 +0200

    there are some issues in the doppler processing - these changes are for debugging..

--- nansencloud/processing_sar_doppler/utils.py
+++ nansencloud/processing_sar_doppler/utils.py
@@ -1,7 +1,7 @@
 '''
 Utility functions for processing Doppler from multiple SAR acquisitions
 '''
-import os, datetime
+import os, datetime, warnings
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize
@@ -24,7 +24,8 @@ t1 = datetime.datetime(2010,1,3,0,0,0, tzinfo=timezone.utc)
 def rb_model_func(x, a, b, c, d, e, f):
     return a + b*x + c*x**2 + d*x**3 + e*np.sin(x) + f*np.cos(x)
 
-def update_geophysical_doppler(t0, t1, swath, sensor='ASAR', platform='ENVISAT'):
+def update_geophysical_doppler(t0, t1, swath, sensor='ASAR',
+        platform='ENVISAT'):
 
     # Get datasets
     DS = Dataset.objects.filter(source__platform__short_name=platform,
@@ -38,14 +39,24 @@ def update_geophysical_doppler(t0, t1, swath, sensor='ASAR', platform='ENVISAT')
     swath_files = []
     for dd in dopDS:
         try:
-            swath_files.append(
-                    dd.dataseturi_set.get(
-                        uri__endswith='subswath%s.nc' %swath).uri
-                )
+            fn = dd.dataseturi_set.get(
+                    uri__endswith='subswath%s.nc' %swath).uri
+            n = Nansat(fn)
+            lon,lat=n.get_geolocation_grids()
+            indmidaz = lat.shape[0]/2
+            indmidra = lat.shape[1]/2
+            if lat[indmidaz,indmidra]>lat[0,indmidra]:
+                orbit_pass = 'ascending'
+                # Get rid of ascending pass now...
+                warnings.warn('Get rid of ascending pass now: %s' %fn)
+                dd.delete()
+            else:
+                swath_files.append(fn)
         except DatasetURI.DoesNotExist:
             continue
 
     valid_land = np.array([])
+    valid = np.array([])
     for ff in swath_files:
         n = Nansat(ff)
         view_bandnum = n._get_band_number({
@@ -56,6 +67,38 @@ def update_geophysical_doppler(t0, t1, swath, sensor='ASAR', platform='ENVISAT')
                 'standard_deviation_of_surface_backwards_doppler_centroid_frequency_shift_of_radar_wave',
         })
         pol = n.get_metadata(bandID=std_bandnum, key='polarization')
+
+        # For checking when antenna pattern changes
+        if valid.shape==(0,):
+            valid = n['valid_doppler']
+            dca0 = n['dca']
+            dca0[n['valid_doppler']==0] = np.nan
+            dca0[n['valid_sea_doppler']==1] = dca0[n['valid_sea_doppler']==1] - \
+                    n['fww'][n['valid_sea_doppler']==1]
+            view_angle0 = n[view_bandnum]
+        else:
+            validn = n['valid_doppler']
+            dca0n = n['dca']
+            dca0n[n['valid_doppler']==0] = np.nan
+            dca0n[n['valid_sea_doppler']==1] = dca0n[n['valid_sea_doppler']==1] - \
+                    n['fww'][n['valid_sea_doppler']==1]
+            view_angle0n = n[view_bandnum]
+            if not validn.shape==valid.shape:
+                if validn.shape[1] > valid.shape[1]:
+                    valid = np.resize(valid, (valid.shape[0], validn.shape[1]))
+                    dca0 = np.resize(dca0, (dca0.shape[0], dca0n.shape[1]))
+                    view_angle0 = np.resize(view_angle0,
+                        (view_angle0.shape[0], view_angle0n.shape[1]))
+                else:
+                    validn = np.resize(validn, (validn.shape[0],
+                        valid.shape[1]))
+                    dca0n = np.resize(dca0n, (dca0n.shape[0], dca0.shape[1]))
+                    view_angle0n = np.resize(view_angle0n,
+                        (view_angle0n.shape[0], view_angle0.shape[1]))
+            valid = np.concatenate((valid, validn))
+            dca0 = np.concatenate((dca0, dca0n))
+            view_angle0 = np.concatenate((view_angle0, view_angle0n))
+
 
         if valid_land.shape==(0,):
             valid_land = n['valid_land_doppler'][n['valid_land_doppler'].any(axis=1)]
@@ -89,6 +132,14 @@ def update_geophysical_doppler(t0, t1, swath, sensor='ASAR', platform='ENVISAT')
             view_angle = np.concatenate((view_angle, view_angle_n))
             std_dca = np.concatenate((std_dca, std_dca_n))
 
+    view_angle0 = view_angle0.flatten()
+    dca0 = dca0.flatten()
+    view_angle0 = np.delete(view_angle0, np.where(np.isnan(dca0)))
+    dca0 = np.delete(dca0, np.where(np.isnan(dca0)))
+    ind = np.argsort(view_angle0)
+    view_angle0 = view_angle0[ind]
+    dca0 = dca0[ind]
+
     # Set dca, view_angle and std_dca to nan where not land
     dca[valid_land==0] = np.nan
     std_dca[valid_land==0] = np.nan
@@ -116,9 +167,16 @@ def update_geophysical_doppler(t0, t1, swath, sensor='ASAR', platform='ENVISAT')
         }
 
     # Show this in presentation:
+    plt.subplot(1,2,1)
+    count, anglebins, dcabins, im = plt.hist2d(view_angle0, dca0, 100, cmin=1,
+            range=[[np.min(view_angle), np.max(view_angle)], freqLims[swath]])
+    plt.colorbar()
+    plt.title('Wind Doppler subtracted')
+    plt.subplot(1,2,2)
     count, anglebins, dcabins, im = plt.hist2d(view_angle, dca, 100, cmin=1,
             range=[[np.min(view_angle), np.max(view_angle)], freqLims[swath]])
     plt.colorbar()
+    plt.title('Doppler over land')
     plt.show()
     #plt.close()
     countLims = {
@@ -195,6 +253,7 @@ def update_geophysical_doppler(t0, t1, swath, sensor='ASAR', platform='ENVISAT')
     #plt.show()
 
 
+    land = np.array([])
     for ff in swath_files:
         start_time = time.time()
         n = Doppler(ff)
@@ -212,6 +271,33 @@ def update_geophysical_doppler(t0, t1, swath, sensor='ASAR', platform='ENVISAT')
             parameters={'wkv':
                 'surface_backwards_doppler_frequency_shift_of_radar_wave_due_to_surface_velocity'}
             )
+
+        current = -(np.pi*(fdg - n['fww']) / 112 /
+                np.sin(n['incidence_angle']*np.pi/180))
+        import ipdb
+        ipdb.set_trace()
+        n.add_band(array=current,
+                parameters={'name': 'current', 'units': 'm/s', 'minmax': '-2 2'}
+            )
+
+        # add land data for accuracy calculation
+        if land.shape==(0,):
+            land = n['valid_land_doppler'][n['valid_land_doppler'].any(axis=1)]
+            land_fdg = fdg[n['valid_land_doppler'].any(axis=1)]
+        else:
+            landn = n['valid_land_doppler'][n['valid_land_doppler'].any(axis=1)]
+            land_fdgn = fdg[n['valid_land_doppler'].any(axis=1)]
+            if not landn.shape==land.shape:
+                if landn.shape[1] > land.shape[1]:
+                    land = np.resize(land, (land.shape[0], landn.shape[1]))
+                    land_fdg = np.resize(land_fdg, (land_fdg.shape[0],
+                        land_fdgn.shape[1]))
+                if landn.shape[1] < land.shape[1]:
+                    landn = np.resize(landn, (landn.shape[0], land.shape[1]))
+                    land_fdgn = np.resize(land_fdgn, (land_fdgn.shape[0],
+                        land.shape[1]))
+            land = np.concatenate((land, landn))
+            land_fdg = np.concatenate((land_fdg, land_fdgn))
 
         module = 'nansencloud.processing_sar_doppler'
         DS = Dataset.objects.get(dataseturi__uri=n.fileName)
@@ -248,7 +334,20 @@ def update_geophysical_doppler(t0, t1, swath, sensor='ASAR', platform='ENVISAT')
                 mask_lut={0:[128,128,128]}, transparency=[128,128,128])
         print("--- %s seconds ---" % (time.time() - start_time))
 
+    land_fdg[land==0] = np.nan
+    print('Standard deviation over land: %.2f' %np.nanstd(land_fdg))
 
+    #dca[valid_land==0] = np.nan
+    #std_dca[valid_land==0] = np.nan
+    #view_angle[valid_land==0] = np.nan
+
+    #dca = dca.flatten()
+    #std_dca = std_dca.flatten()
+    #view_angle = view_angle.flatten()
+
+    #dca = np.delete(dca, np.where(np.isnan(dca)))
+    #std_dca = np.delete(std_dca, np.where(np.isnan(std_dca)))
+    #view_angle = np.delete(view_angle, np.where(np.isnan(view_angle)))
 
 
 
