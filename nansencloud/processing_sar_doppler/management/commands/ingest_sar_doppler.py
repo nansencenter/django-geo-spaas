1fa7ff1923840fc7f587a1cf68b9e396a786be2c
mortenwh <morten.wergeland.hansen@gmail.com>
2016-07-05 14:54:11 +0200

    Give correct output..

--- nansencloud/processing_sar_doppler/management/commands/ingest_sar_doppler.py
+++ nansencloud/processing_sar_doppler/management/commands/ingest_sar_doppler.py
@@ -1,6 +1,8 @@
 ''' Processing of SAR Doppler from Norut's GSAR '''
 from optparse import make_option
 
+from nansat.tools import GeolocationError
+
 from django.core.management.base import BaseCommand
 
 from nansencloud.utils import uris_from_args
@@ -22,8 +24,15 @@ class Command(BaseCommand):
 
         for non_ingested_uri in uris_from_args(*args):
             self.stdout.write('Ingesting %s ...\n' % non_ingested_uri)
-            ds, cr = Dataset.objects.get_or_create(non_ingested_uri, **options)
-            self.stdout.write('Successfully added: %s\n' % non_ingested_uri)
+            try:
+                ds, cr = Dataset.objects.get_or_create(non_ingested_uri, **options)
+            except GeolocationError:
+                self.stdout.write('Geolocation error in: %s\n' % non_ingested_uri)
+                continue
+            if cr:
+                self.stdout.write('Successfully added: %s\n' % non_ingested_uri)
+            else:
+                self.stdout.write('Was already added: %s\n' % non_ingested_uri)
 
 
 
