e9cadea75e53524de816beb165a323da02aaae7a
mortenwh <morten.wergeland.hansen@gmail.com>
2017-06-20 19:21:01 +0200

    changed path of ncep grib file to correct mount dir

--- geospaas/processing_sar_doppler/tests.py
+++ geospaas/processing_sar_doppler/tests.py
@@ -8,10 +8,10 @@ class TestProcessingSARDoppler(TestCase):
 
     def test_process_sar_doppler(self):
         out = StringIO()
-        wf = 'file://localhost/mnt/10.11.12.232/sat_auxdata/model/ncep/gfs/' \
+        wf = 'file://localhost/mnt/10.11.12.231/sat_auxdata/model/ncep/gfs/' \
                 'gfs20091116/gfs.t18z.master.grbf03'
         call_command('ingest', wf, stdout=out)
-        f = 'file://localhost/mnt/10.11.12.232/sat_downloads_asar/level-0/' \
+        f = 'file://localhost/mnt/10.11.12.231/sat_downloads_asar/level-0/' \
                 'gsar_rvl/RVL_ASA_WS_20091116195940116.gsar'
         call_command('ingest_sar_doppler', f, stdout=out)
         self.assertIn('Successfully added:', out.getvalue())
