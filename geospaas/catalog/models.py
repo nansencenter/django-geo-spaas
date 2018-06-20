724d2bc16d4e7017e7b9ec1ac1c4eb135e196a88
mortenwh <morten.wergeland.hansen@gmail.com>
2018-06-20 14:46:03 +0200

    changed model field following https://docs.djangoproject.com/en/1.11/howto/writing-migrations/

--- geospaas/catalog/models.py
+++ geospaas/catalog/models.py
@@ -122,7 +122,7 @@ class Dataset(models.Model):
         )
 
     # DIF required fields
-    entry_id = models.CharField(max_length=80, unique=True, default=None, 
+    entry_id = models.CharField(max_length=80, unique=True, default=uuid.uuid4, 
         validators=[
             RegexValidator(r'^[0-9a-zA-Z_.-]*$', 'Only alphanumeric characters are allowed.')
         ]
