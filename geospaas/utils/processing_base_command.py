826ca8b73f9592cd678b15ec1ee60b22920df689
akorosov <korosov@gmail.com>
2018-08-02 17:38:54 +0200

    #17 update base command and tests

--- geospaas/utils/processing_base_command.py
+++ geospaas/utils/processing_base_command.py
@@ -3,19 +3,18 @@ import glob
 from datetime import datetime as dt
 
 from django.db.models import Q
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.contrib.gis.geos import GEOSGeometry, Polygon, GEOSException
 from django.contrib.gis.gdal import GDALException
 
 from geospaas.catalog.models import Dataset
 
 def valid_date(s):
-    ''' Test input datestring '''
+    """ Validate input datestring """
     try:
         return dt.strptime(s, "%Y-%m-%d")
-    except ValueError:
-        msg = "Not a valid date: '{0}'.".format(s)
-        raise TypeError(msg)
+    except:# ValueError:
+        raise CommandError("Not a valid date: '{0}'.".format(s))
 
 
 class ProcessingBaseCommand(BaseCommand):
@@ -40,15 +39,15 @@ class ProcessingBaseCommand(BaseCommand):
                             action='store',
                             metavar='YYYY-MM-DD',
                             default='1900-01-01',
-                            help='Start of time range',
-                            type=valid_date)
+                            help='Start of time range',)
+                            #type=valid_date) # such validation doesn't work with Django ...
 
         parser.add_argument('--end',
                             action='store',
                             metavar='YYYY-MM-DD',
                             default='2100-12-31',
-                            help='End of time range',
-                            type=valid_date)
+                            help='End of time range',)
+                            #type=valid_date) # such validation doesn't work with Django ...
 
         parser.add_argument('--extent',
                             action='store',
@@ -60,7 +59,7 @@ class ProcessingBaseCommand(BaseCommand):
 
         parser.add_argument('--geojson',
                             action='store',
-                            metavar='GeoJSON',
+                            metavar='FILENAME',
                             default=None,
                             help='Filename of GeoJSON with polygon for spatial search',
                             type=str)
@@ -68,17 +67,17 @@ class ProcessingBaseCommand(BaseCommand):
         parser.add_argument('--mask',
                             action='store',
                             default='',
-                            help='Wildcard for filtering on filenames',
+                            help='Return only the datasets that have MASK in filename',
                             type=str)
 
         parser.add_argument('--force', action='store_true',
-                            help='Force processing')
+                            help='Force processing of all found datasets')
 
         parser.add_argument('--limit',
                             action='store',
                             default=None,
                             type=int,
-                            help='Number of entries to process')
+                            help='Number of found datasets to process')
 
     def geometry_from_options(self, extent=None, geojson=None, **kwargs):
         """ Generate geometry to use in spatial search
@@ -101,7 +100,6 @@ class ProcessingBaseCommand(BaseCommand):
         geometry : geos.Polygon or None
         """
         polygon = None
-
         if extent is not None:
             # generate polygon from input extent parameter
             polygon = Polygon([ [extent[0], extent[2]],
@@ -109,45 +107,54 @@ class ProcessingBaseCommand(BaseCommand):
                                 [extent[1], extent[3]],
                                 [extent[0], extent[3]],
                                 [extent[0], extent[2]]])
-        elif geojson is not None and os.path.exists(geojson):
+        elif geojson is not None:
+            if not os.path.exists(geojson):
+                raise CommandError('GeoJSON file %s was not found!' % geojson)
             # try to load GeoJSON and generate valid polygon
             with open(geojson, 'rt') as f:
                 geojson = f.read().strip()
             try:
                 polygon = GEOSGeometry(geojson)
             except (ValueError, GDALException, GEOSException):
-                raise ValueError('Failed to read valid GeoJSON from %s'%options['geojson'])
-            if not isinstance(polygon, Polygon):
-                raise ValueError('Incorrect geometry type in GeoJSON %s'%options['geojson'])
+                raise CommandError('Failed to read valid GeoJSON from %s' % geojson)
 
         return polygon
 
-    def find_datasets(self, **options):
+    def find_datasets(self, start='1900-01-01',
+                            end='2010-12-31',
+                            extent=None,
+                            geojson=None,
+                            mask='',
+                            **kwargs):
         """ Find datasets that match input parameters
 
         Parameters
         ----------
-        options : dict
-            start : datetime
-            end : datetime
-            mask : str
-            force : bool
-            limit : int
+        start : str
+            Start of time range 'YYYY-MM-DD'
+        end : str
+            End of time range 'YYYY-MM-DD'
+        extent : [float, float, float, float]
+            Spatial extent ('MIN_LON', 'MAX_LON', 'MIN_LAT', 'MAX_LAT')
+        geojson : str
+            Filename of GeoJSON with polygon for spatial search
+        mask : str
+            Return only the datasets that have MASK in filename
 
         Returns
         -------
             datasets : django.QuerySet
+
         """
-
+        start = valid_date(start)
+        end = valid_date(end)
         datasets = Dataset.objects.filter(
-                time_coverage_start__gte=options['start'],
-                time_coverage_start__lte=options['end'],
-                dataseturi__uri__contains=options['mask']).order_by('time_coverage_start')
+                time_coverage_start__gte=start,
+                time_coverage_start__lte=end,
+                dataseturi__uri__contains=mask).order_by('time_coverage_start')
 
-        geometry = self.geometry_from_options(**options)
+        geometry = self.geometry_from_options(extent=extent, geojson=geojson)
         if geometry is not None:
-            datasets = datasets.filter(
-                (Q(geographic_location__geometry__overlaps=geometry) |
-                 Q(geographic_location__geometry__within=geometry)))
+            datasets = datasets.filter(geographic_location__geometry__intersects=geometry)
 
         return datasets
