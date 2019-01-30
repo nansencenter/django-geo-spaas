4e92bd29c1156ea3ac75ba5b5eb1080be5257ac2
jeong1park <orepaku.android@gmail.com>
2019-01-30 14:05:46 +0100

    #34 optional init_project

--- init_project.sh
+++ init_project.sh
@@ -2,12 +2,16 @@
 
 # set project name
 PROJECT_NAME=project
+if [ ! -d "$PROJECT_NAME" ]; then
 
-# create project dir
-django-admin startproject $PROJECT_NAME
-# copy default settings
-cp tests/*.py $PROJECT_NAME/$PROJECT_NAME/
-# migrate data to database
-python $PROJECT_NAME/manage.py migrate
-# add metadata to Vocabularies
-python $PROJECT_NAME/manage.py update_vocabularies
+    # Control will enter here if $DIRECTORY doesn't exist.
+    # create project dir
+    django-admin startproject $PROJECT_NAME
+    # copy default settings
+    cp tests/*.py $PROJECT_NAME/$PROJECT_NAME/
+    # migrate data to database
+    python $PROJECT_NAME/manage.py migrate
+    # add metadata to Vocabularies
+    python $PROJECT_NAME/manage.py update_vocabularies
+
+fi
