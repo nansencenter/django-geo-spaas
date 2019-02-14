ba43645e0f9bb9acea1e6632dc232a9bc968581c
mortenwh <morten.wergeland.hansen@gmail.com>
2019-02-14 15:29:06 +0100

    #24: mock request method so it returns object with status 200

--- geospaas/tests.py
+++ geospaas/tests.py
@@ -1,17 +1,11 @@
 import os
-from mock import patch
+from mock import patch, PropertyMock
 
 from django.test import TestCase
 
 from geospaas.utils import utils
 
 class TestUtils(TestCase):
-
-    def test_validate_uri_opendap(self):
-        uri = 'http://www.ifremer.fr/opendap/cerdap1/globcurrent/' \
-                'v2.0/global_012_deg/geostrophic/2014/001/' \
-                '20140101000000-GLOBCURRENT-L4-CURgeo_0m-ALT_OI-v02.0-fv01.0.nc'
-        self.assertEqual(utils.validate_uri(uri), None)
 
     def test_validate_uri_opendap_does_not_exist(self):
         uri = 'http://www.ifremer.fr/opendap/cerdap1/globcurrent/' \
@@ -33,3 +27,10 @@ class TestUtils(TestCase):
             utils.validate_uri(uri)
         the_exception = '/some/folder/filename.ext'
         self.assertEqual(the_exception, cm.exception.args[0])
+
+    @patch('urllib3.PoolManager')
+    def test__validate_uri__opendap_exists(self, mock_PoolManager):
+        # Mock request.status so it returns 200, meaning successful connection..
+        mock_PoolManager.return_value.request.return_value = PropertyMock(status=200)
+        uri = 'http://nbstds.met.no/thredds/catalog/NBS/S2A/test_catalog.html'
+        self.assertEqual(utils.validate_uri(uri), None)
