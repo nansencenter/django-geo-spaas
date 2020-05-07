660f70d60cf8a6f8ad3f268e5136c9c9712719ca
aanersc <arash.azamifard@nersc.no>
2020-05-07 10:52:20 +0200

    #92 adding a new app into geospass with new models

---
+++ geospaas/base_viewer/forms.py
@@ -0,0 +1,29 @@
+from django.contrib.gis import forms
+from django.utils import timezone
+
+from geospaas.viewer.models import Search
+
+class SearchForm(forms.ModelForm):
+
+    class Meta:
+        #model = TestSearch
+        #fields = ['date0', 'date1', 'source']
+        model = Search
+        fields = ['polygon', 'date0', 'date1', 'source']
+        labels = {'polygon':''}
+        widgets = {'polygon': LeafletWidget(
+                attrs={'settings_overrides':
+                        {'DEFAULT_CENTER': (60.0, 5.0),
+                        'DEFAULT_ZOOM': 1,
+                        'PLUGINS': {'forms': {'auto-include': True}},
+                         }}
+                        ),
+                   'date0': forms.DateInput(attrs={'class':'datepicker'}),
+                   'date1': forms.DateInput(attrs={'class':'datepicker'}),}
+
+    def __init__(self, *args, **kwargs):
+        initial = kwargs.pop('initial', {})
+        initial['date0'] = timezone.datetime(2000, 1, 1,
+                    tzinfo=timezone.utc).date(),
+        initial['date1'] = timezone.now().date()
+        super().__init__(initial=initial, *args, **kwargs)
---
+++ geospaas/base_viewer/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.gis.db import models as geomodels
+from geospaas.catalog.models import Source as CatalogSource
+from geospaas.catalog.models import DatasetParameter as CatalogDatasetParameter
+# Create your models here.
+class Search(geomodels.Model):
+    ''' Search without parameters '''
+    sdate = models.DateTimeField() # when was search
+    date0 = models.DateField()
+    date1 = models.DateField()
+    source = models.ManyToManyField(CatalogSource, blank=True)#, null=True)
+
+    # GeoDjango-specific: a geometry field (PolygonField), and
+    # overriding the default manager with a GeoManager instance.
+    polygon = geomodels.PolygonField(blank=True, null=True) # intersect this poly
+
+class Search_with_para(Search):
+    ''' Search parameters '''
+    DatasetParameter = models.ForeignKey(CatalogDatasetParameter, on_delete=models.CASCADE)
