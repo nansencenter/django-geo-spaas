17058e7622f037792230d367ec92544a634671c6
aanersc <arash.azamifard@nersc.no>
2020-05-12 13:10:57 +0200

    #92 for show only

--- geospaas/base_viewer/views.py
+++ geospaas/base_viewer/views.py
@@ -1,63 +1,89 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.views.generic import View
-from geospaas.base_viewer.forms import SearchForm
-from geospaas.base_viewer.forms import Search_with_para_Form
+from geospaas.base_viewer.forms import SearchFormBelow, SearchFormAbove
+from geospaas.catalog.models import Dataset as CatalogDataset
+#from geospaas.base_viewer.forms import Search_with_para_Form
 # Create your views here.
+
+
 class IndexView(View):
-    form_class = SearchForm
+
+    # order of this list matters for showing propery in the templates with correct order
+    form_class = [SearchFormAbove, SearchFormBelow, ]
+
     main_template = 'base_viewer/template_for_base.html'
     viewname = 'index'
-    form = None
+    forms = list()
     context = {}
 
-    def set_form_defaults(self, request):
-        ''' Set default values for the form '''
-        self.form = self.form_class(
-                    {'date0' :
-                        timezone.datetime(2000,1,1,tzinfo=timezone.utc).date(),
-                     'date1' : timezone.now().date()})
+    def instantiation_and_set_form_defaults(self):
+        ''' Set default values for the form by instantiating them '''
+        # initialize the form list for setting default values
+        self.forms = [None] * len(self.form_class)
+        # loop for instantiating with defaults
+        for counter, element_form_class in enumerate(self.form_class):
+            # similar default values can be set via *elif* at the bottom of this statement
+            if 'time_coverage_start' in element_form_class.Meta.fields:
+                self.forms[counter] = element_form_class(
+                    {'time_coverage_start':
+                     timezone.datetime(2000, 1, 1, tzinfo=timezone.utc).date(),
+                     'time_coverage_end': timezone.now().date(),
+                     })
+            # elif: ######## code here for other default values
+                    ######## code here for other default values
+            else:  # instantiating without default values
+                self.forms[counter] = element_form_class({})
+
 
     def get(self, request, *args, **kwargs):
         ''' Render page if no data is given '''
         # set default values of form
-        self.set_form_defaults(request)
-        self.form.is_valid()
+        self.instantiation_and_set_form_defaults()
+        self.validation_and_error_check()
 
         # modify attributes based on self.form
-        return self.render_func(request)
+        return self.sorting_func(request)
 
     def post(self, request, *args, **kwargs):
-        ''' Render page is some data is given in the form '''
-        # set default values of form
+        ''' feed the post data in the form (up to obtain the cleaned data from post)'''
+        # initialize the form list for setting default values
+        self.forms = [None] * len(self.form_class)
 
-        # create new form from POST data
-        form = self.form_class(request.POST)
+        # loop for instantiating with data of POST request
+        for counter, element_form_class in enumerate(self.form_class):
+            self.forms[counter] = element_form_class(request.POST)
 
-        # replace erroneous data in the form with default values
-        if not form.is_valid():
-            for errorField in form.errors:
-                form.cleaned_data[errorField] = self.form[errorField]
 
-        # keep the query
-        if form.is_valid():
-            s = form.save(commit=False)
-            s.sdate = timezone.now().date()
-            s.save()
-
-        # keep the form in self
-        self.form = form
-        self.form.is_valid()
-
+        self.validation_and_error_check()
         # modify attributes based on self.form
-        return self.render_func(request)
+        return self.sorting_func(request)
+
+    def validation_and_error_check(self):
+        for element_forms in self.forms:  # for loop for making clean data by is_valid() method
+            element_forms.is_valid()
+            for errorField in element_forms.errors: # temporary error message
+                print(f"errorField")
+
+    def sorting_func(self, request):
+        ds = CatalogDataset
+        # filter by date
+        #ds = blabla(ds)
+        # filter by source
+        # ds = blabla(ds)
+        # filter by geometry
+        # ds = blabla(ds)
+        return self.final_rendering(request,ds)
 
 
-    def render_func(self, request):
-        ''' Render page based on form data '''
-        # filter datasets
-        #self.context['dsp'] = DatasetParameter
-        #self.context['datasets'] = datasets
-
-        self.context['formi'] = self.form
+    def final_rendering(self, request,ds):
+        ''' Render page based on several forms of data as well as some other context '''
+        self.context = {}  # initializing the contect for templates
+        # initializing the list of the forms that passed into context
+        form_list = [None] * len(self.forms)
+        for counter, value in enumerate(self.forms):
+            # make the form_list elements one by one (form by form)
+            form_list[counter] = value
+        # passing the form_list into context
+        self.context['form_list'] = form_list
         return render(request, self.main_template, self.context)
