eff1ceb0b54a5bd0168a37dd56a8213b5b31db49
aanersc <arash.azamifard@nersc.no>
2020-05-14 12:54:22 +0200

    #92 form-agnostic development

--- geospaas/base_viewer/forms.py
+++ geospaas/base_viewer/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.gis import forms
 from django.utils import timezone
+from django.db.models import Q
+
 from leaflet.forms.widgets import LeafletWidget
 from geospaas.catalog.models import Dataset as CatalogDataset
 from django.contrib.gis.db import models as geomodels
@@ -8,19 +10,23 @@ from geospaas.base_viewer.models import Search_dummy_leaflet
 #from geospaas.base_viewer.models import Search_with_para
 class SearchFormBelow(forms.ModelForm):
     class Meta:
-
         model = CatalogDataset
         fields = ['time_coverage_start','time_coverage_end','source']
 
-    def __init__(self, *args, **kwargs):
-        #self.geographic_location = CatalogGeographicLocation
-        initial = kwargs.pop('initial', {})
-        initial['time_coverage_start'] = timezone.datetime(2000, 1, 1,
-                    tzinfo=timezone.utc).date(),
-        initial['time_coverage_end'] = timezone.now().date()
-        initial['source'] = CatalogDataset.objects.first().source
-        super().__init__(initial=initial, *args, **kwargs)
+    def set_defaults(self):
+        self.data['time_coverage_end'] = timezone.now().date()
+        self.data['time_coverage_start'] = timezone.datetime(2000, 1, 1,
+                    tzinfo=timezone.utc).date()
+        self.data['source'] = CatalogDataset.objects.first().source
 
+    def filter(self,ds):
+        t0 = self.cleaned_data['time_coverage_start']
+        t1 = self.cleaned_data['time_coverage_end'] + timezone.timedelta(hours=24)
+        ds = ds.filter(Q(time_coverage_end__lt=t1) & Q(time_coverage_start__gt=t0))
+        src=[]
+        src.append(self.cleaned_data['source'])
+        ds = ds.filter(source__in=src)
+        return ds
 
 class SearchFormAbove(forms.ModelForm):
     class Meta:
@@ -34,6 +40,18 @@ class SearchFormAbove(forms.ModelForm):
                         'PLUGINS': {'forms': {'auto-include': True}},
                          }}
                         )}
+    def set_defaults(self):
+        pass
+
+    def filter(self,ds):
+        received_polygon= self.cleaned_data['polygon']
+        if received_polygon is not None:
+            ds = ds.filter(geographic_location__geometry__intersects= received_polygon)
+            return ds
+        else:
+            # in the case that user have no specified polygon
+            # there is no need to filter
+            return ds
 
 #class Search_with_para_Form(SearchForm_below):
 #   fields = SearchForm.Meta.fields.append('DatasetParameter')
--- geospaas/base_viewer/views.py
+++ geospaas/base_viewer/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.utils import timezone
 from django.db.models import Q
 from django.views.generic import View
 from geospaas.base_viewer.forms import SearchFormBelow, SearchFormAbove
@@ -24,30 +23,18 @@ class IndexView(View):
         self.forms = [None] * len(self.form_class)
         # loop for instantiating with defaults
         for counter, element_form_class in enumerate(self.form_class):
-            # similar default values can be set via *elif* at the bottom of this statement
-            if 'time_coverage_start' in element_form_class.Meta.fields:
-                self.forms[counter] = element_form_class(
-                    {'time_coverage_start':
-                     timezone.datetime(2000, 1, 1, tzinfo=timezone.utc).date(),
-                     'time_coverage_end': timezone.now().date(),
-                     })
-            # elif: ######## code here for other default values
-                    ######## code here for other default values
-            else:  # instantiating without default values
-                self.forms[counter] = element_form_class({})
-
+            self.forms[counter] = element_form_class({})  # instantiation
+            self.forms[counter].set_defaults()  # setting the defaults
 
     def get(self, request, *args, **kwargs):
         ''' Render page if no data is given '''
-        # set default values of form
         self.instantiation_and_set_form_defaults()
         self.validation_and_error_check()
-
-        # modify attributes based on self.form
-        return self.sorting_func(request)
+        self.filtering_func(request)
+        return self.final_rendering(request)
 
     def post(self, request, *args, **kwargs):
-        ''' feed the post data in the form (up to obtain the cleaned data from post)'''
+        ''' all sections needed for POST requests'''
         # initialize the form list for setting default values
         self.forms = [None] * len(self.form_class)
 
@@ -56,50 +43,34 @@ class IndexView(View):
             self.forms[counter] = element_form_class(request.POST)
 
         self.validation_and_error_check()
-        # modify attributes based on self.form
-        return self.sorting_func(request)
+
+        # modify attributes based on the forms cleaned data
+        self.filtering_func(request)
+        return self.final_rendering(request)
 
     def validation_and_error_check(self):
         for element_forms in self.forms:  # for loop for making clean data by is_valid() method
             element_forms.is_valid()
-            for errorField in element_forms.errors: # temporary error message
+            for errorField in element_forms.errors:  # temporary error message
                 print(f"errorField")
 
-    def sorting_func(self, request):
-        self.ds = CatalogDataset.objects.all()
-        self.filtering_func()
-        return self.final_rendering(request)
-
+    def filtering_func(self, request):
+        ds = CatalogDataset.objects.all()
+        if (request.method == 'POST'):
+            for counter in range(len(self.form_class)):
+                # using the filter function of each form sequentially
+                ds = self.forms[counter].filter(ds)
+            self.ds = ds
+        elif (request.method == 'GET'):
+            self.ds = ds
 
     def final_rendering(self, request):
         ''' Render page based on several forms of data as well as some other context '''
+        self.context = {}
         self.set_context()
         return render(request, self.main_template, self.context)
 
-
-# functions to be overloaded in child classes
-
-    def filtering_func(self):
-        # filter by source
-        src=[]
-        for element_forms in self.forms:
-            if 'source' in element_forms.cleaned_data:
-                if element_forms.cleaned_data['source'] is not None:
-                    src.append(element_forms.cleaned_data['source'])
-
-        self.ds = self.ds.filter(source__in=src)
-        # filter by time
-        t0 = self.forms[1].cleaned_data['time_coverage_start']
-        t1 = self.forms[1].cleaned_data['time_coverage_end'] + timezone.timedelta(hours=24)
-        self.ds = self.ds.filter(Q(time_coverage_end__lt=t1) & Q(time_coverage_start__gt=t0))
-        # filter by geometry
-        if self.forms[0].cleaned_data['polygon'] is not None:
-            self.ds = self.ds.filter(geographic_location__geometry__intersects= self.forms[0].cleaned_data['polygon'])
-        # ds = blabla(ds)
-        #return self.ds
-
     def set_context(self):
-        self.context = {}  # initializing the contect for templates
         # initializing the list of the forms that passed into context
         form_list = [None] * len(self.forms)
         for counter, value in enumerate(self.forms):
@@ -107,4 +78,7 @@ class IndexView(View):
             form_list[counter] = value
         # passing the form_list into context
         self.context['form_list'] = form_list
+        self.set_dataset_context()
+
+    def set_dataset_context(self): # excluded from other context for pagination development of django
         self.context['datasets'] = self.ds
