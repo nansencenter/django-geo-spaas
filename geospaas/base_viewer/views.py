e7953700a0d2434d9ae0db11f6f11271c7e7fca9
aanersc <arash.azamifard@nersc.no>
2020-05-20 08:39:01 +0200

    #92 change the way of passing the context into templates

--- geospaas/base_viewer/views.py
+++ geospaas/base_viewer/views.py
@@ -4,50 +4,24 @@ from django.views.generic import View
 from geospaas.base_viewer.forms import TimeAndSourceForm, SpatialSearchForm
 from geospaas.catalog.models import Dataset as CatalogDataset
 
-#from geospaas.base_viewer.forms import Search_with_para_Form
 # Create your views here.
 
 
 class IndexView(View):
 
-    form_class = [ SpatialSearchForm, TimeAndSourceForm,]
+    form_class = [TimeAndSourceForm, SpatialSearchForm,]
     main_template = 'base_viewer/template_for_base.html'
     viewname = 'index'
-    forms = list()
+    #forms = list()
 
     @classmethod
-    def create_forms(cls, *args):#request_method='GET'
+    def create_forms(cls, *args):  # request_method='GET'
         ''' Set default values for the form by instantiating them '''
         return [i(*args) for i in cls.form_class]
 
-    def get(self, request, *args, **kwargs):
-        ''' Render page if no data is given '''
-        forms = self.create_forms()
-        forms = self.validate_forms(forms)
-        #self.forms = forms
-        ds = self.get_all_datasets()
-        self.set_context(forms,ds)
-        return render(request, self.main_template, self.context)
-
-    def post(self, request, *args, **kwargs):
-        ''' all sections needed for POST requests'''
-
-        #### instantiating with data of POST request
-        ###self.forms = [i(request.POST) for i in self.form_class]
-        #self.create_forms(request.POST)
-        forms = self.create_forms(request.POST)
-
-        forms = self.validate_forms(forms)
-        # modify ds based on the forms cleaned data
-        ds = self.get_filtered_datasets(forms)
-
-        # return self.final_rendering(request)
-        self.set_context(forms,ds)
-        return render(request, self.main_template, self.context)
-
-    def get_all_datasets(self):
+    @classmethod
+    def get_all_datasets(cls):
         return CatalogDataset.objects.all()
-
 
     @staticmethod
     def validate_forms(forms):
@@ -55,20 +29,45 @@ class IndexView(View):
             form.is_valid()
         return forms
 
-    def get_filtered_datasets(self,forms):
-        ds = self.get_all_datasets()
+    @classmethod
+    def get_filtered_datasets(cls, forms):
+        ds = cls.get_all_datasets()
         for form in forms:
             # using the filter function of each form sequentially
             ds = form.filter(ds)
         return ds
 
-    def set_context(self,forms,ds):
-        self.context = {}
-        # initializing the list of the forms that passed into context
-        # passing the form_list into context
-        self.context['form_list'] = forms
-        self.set_dataset_context(ds)
+    @classmethod
+    def set_context(cls, forms, ds):
+        context = {}
+        # setting the forms based on their class into the correct naming in the context
+        for j in cls.form_class:
+            for form in forms:
+                if (isinstance(form.__class__(), j )):
+                    context[j.__name__] = form
 
-    # excluded from other context for pagination development of django
-    def set_dataset_context(self,ds):
-        self.context['datasets'] = ds
+        context['datasets'] = cls.set_dataset_context(ds)
+        return context
+
+    @classmethod
+    def set_dataset_context(cls, ds):
+        return ds # excluded from other context for pagination development of django
+
+
+    def get(self, request, *args, **kwargs):
+        ''' Render page if no data is given '''
+        forms = self.create_forms()
+        forms = self.validate_forms(forms)
+        ds = self.get_all_datasets()
+        context = self.set_context(forms, ds)
+        return render(request, self.main_template, context)
+
+    def post(self, request, *args, **kwargs):
+        ''' all sections needed for POST requests'''
+
+        forms = self.create_forms(request.POST)
+        forms = self.validate_forms(forms)
+        # modify ds based on the forms cleaned data
+        ds = self.get_filtered_datasets(forms)
+        context = self.set_context(forms, ds)
+        return render(request, self.main_template, context)
