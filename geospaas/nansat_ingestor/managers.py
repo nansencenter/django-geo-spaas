ccd99a57ddd24d36e01eeefe638e8b0545e854b8
aanersc <arash.azamifard@nersc.no>
2020-07-06 15:13:07 +0200

    #111 addition of swap between the default values and existing ones

--- geospaas/nansat_ingestor/managers.py
+++ geospaas/nansat_ingestor/managers.py
@@ -68,118 +68,79 @@ class DatasetManager(models.Manager):
 
         # get metadata from Nansat and get objects from vocabularies
         n_metadata = n.get_metadata()
-        entry_id = n_metadata['entry_id']
-        if Dataset.objects.filter(entry_id__icontains=entry_id).count() >= 1:
-            qds = Dataset.objects.get(
-                entry_id__icontains=entry_id)  # query of found dataset
-            assert len(qds) == 1
-            ds = qds.first()  # this dataset (which is the first and the only one) must be updated
-            # ds. Here is the place for setting the metadata from the nansat that comes from the localfile
 
-            platform, _ = Platform.objects.get_or_create(
-                json.loads(n_metadata['platform']))
-            instrument, _ = Instrument.objects.get_or_create(
-                json.loads(n_metadata['instrument']))
-            specs = n_metadata.get('specs', '')
-            source, _ = Source.objects.get_or_create(platform=platform,
-                                                     instrument=instrument,
-                                                     specs=specs)
-            ds.source = source  # update source
+        if n_metadata['entry_id'] is not None:
+            entry_id = n_metadata['entry_id']
+        # set compulsory metadata (source)
+        platform, _ = Platform.objects.get_or_create(
+            json.loads(n_metadata['platform']))
+        instrument, _ = Instrument.objects.get_or_create(
+            json.loads(n_metadata['instrument']))
+        specs = n_metadata.get('specs', '')
+        source, _ = Source.objects.get_or_create(platform=platform,
+                                                 instrument=instrument,
+                                                 specs=specs)
 
-            if 'entry_title' in n_metadata and n_metadata['entry_title'] is not None:
-                # update entry_title
-                ds.entry_title = n_metadata['entry_title']
-            if 'summary' in n_metadata and n_metadata['summary'] is not None:
-                ds.summary = n_metadata['summary']  # update summary
-            if 'time_coverage_start' in n_metadata and n_metadata['time_coverage_start'] is not None:
-                # update time_coverage_start
-                ds.time_coverage_start = n_metadata['time_coverage_start']
-            if 'time_coverage_end' in n_metadata and n_metadata['time_coverage_end'] is not None:
-                # update time_coverage_end
-                ds.time_coverage_end = n_metadata['time_coverage_end']
+        default_char_fields = {
+            'entry_id': lambda: 'NERSC_' + str(uuid.uuid4()),
+            'entry_title': lambda: 'NONE',
+            'summary': lambda: 'NONE',
+        }
 
-            if len(n.vrt.dataset.GetGCPs()) > 0:
-                n.reproject_gcps()
-            geolocation = GeographicLocation.objects.get_or_create(
-                geometry=WKTReader().read(n.get_border_wkt(nPoints=n_points)))[0]
-            ds.geographic_location = geolocation
-
-            if 'data_center' in n_metadata and n_metadata['data_center'] is not None:
-                ds.data_center = DataCenter.objects.get_or_create(
-                    json.loads(n_metadata['data_center']))[0]
-            if 'gcmd_location' in n_metadata and n_metadata['gcmd_location'] is not None:
-                ds.gcmd_location = Location.objects.get_or_create(
-                    json.loads(n_metadata['gcmd_location']))[0]
-            if 'iso_topic_category' in n_metadata and n_metadata['iso_topic_category'] is not None:
-                ds.ISO_topic_category = ISOTopicCategory.objects.get_or_create(
-                    json.loads(n_metadata['iso_topic_category']))[0]
-            created = False
-            warnings.warn('Dataset with entry_id %s has been updated!' %
-                          (n_metadata['entry_id']))
-        else:
-            # set compulsory metadata (source)
-            platform, _ = Platform.objects.get_or_create(
-                json.loads(n_metadata['platform']))
-            instrument, _ = Instrument.objects.get_or_create(
-                json.loads(n_metadata['instrument']))
-            specs = n_metadata.get('specs', '')
-            source, _ = Source.objects.get_or_create(platform=platform,
-                                                     instrument=instrument,
-                                                     specs=specs)
-
-            default_char_fields = {
-                'entry_id': lambda: 'NERSC_' + str(uuid.uuid4()),
-                'entry_title': lambda: 'NONE',
-                'summary': lambda: 'NONE',
-            }
-
-            # set optional CharField metadata from Nansat or from default_char_fields
-            options = {}
-            for name in default_char_fields:
-                if name not in n_metadata:
-                    warnings.warn(
-                        '%s is not provided in Nansat metadata!' % name)
+        # set optional CharField metadata from Nansat or from default_char_fields
+        options = {}
+        for name in default_char_fields:
+            if name not in n_metadata:
+                warnings.warn('%s is not provided in Nansat metadata!' % name)
+                #prevent the existing values from overwriten action by those of default values
+                if len(Dataset.objects.filter(entry_id=entry_id))>0:
+                    existing_ds = Dataset.objects.get(entry_id=entry_id)
+                    options[name] = existing_ds.__getattribute__(name)
+                else:
                     options[name] = default_char_fields[name]()
-                else:
-                    options[name] = n_metadata[name]
+            else:
+                options[name] = n_metadata[name]
 
-            default_foreign_keys = {
-                'gcmd_location': {'model': Location,
-                                  'value': pti.get_gcmd_location('SEA SURFACE')},
-                'data_center': {'model': DataCenter,
-                                'value': pti.get_gcmd_provider('NERSC')},
-                'ISO_topic_category': {'model': ISOTopicCategory,
-                                       'value': pti.get_iso19115_topic_category('Oceans')},
-            }
+        default_foreign_keys = {
+            'gcmd_location': {'model': Location,
+                              'value': pti.get_gcmd_location('SEA SURFACE')},
+            'data_center': {'model': DataCenter,
+                            'value': pti.get_gcmd_provider('NERSC')},
+            'ISO_topic_category': {'model': ISOTopicCategory,
+                                   'value': pti.get_iso19115_topic_category('Oceans')},
+        }
 
-            # set optional ForeignKey metadata from Nansat or from default_foreign_keys
-            for name in default_foreign_keys:
-                value = default_foreign_keys[name]['value']
-                model = default_foreign_keys[name]['model']
-                if name not in n_metadata:
-                    warnings.warn(
-                        '%s is not provided in Nansat metadata!' % name)
-                else:
-                    try:
-                        value = json.loads(n_metadata[name])
-                    except:
-                        warnings.warn('%s value of %s  metadata provided in Nansat is wrong!' %
-                                      (n_metadata[name], name))
+        # set optional ForeignKey metadata from Nansat or from default_foreign_keys
+        for name in default_foreign_keys:
+            value = default_foreign_keys[name]['value']
+            model = default_foreign_keys[name]['model']
+            if name not in n_metadata:
+                warnings.warn('%s is not provided in Nansat metadata!' % name)
+            else:
+                try:
+                    value = json.loads(n_metadata[name])
+                except:
+                    warnings.warn('%s value of %s  metadata provided in Nansat is wrong!' %
+                                  (n_metadata[name], name))
+            if len(Dataset.objects.filter(entry_id=entry_id))>0:
+                existing_ds = Dataset.objects.get(entry_id=entry_id)
+                options[name] = existing_ds.__getattribute__(name)
+            else:
                 options[name], _ = model.objects.get_or_create(value)
 
-                # Find coverage to set number of points in the geolocation
-                if len(n.vrt.dataset.GetGCPs()) > 0:
-                    n.reproject_gcps()
-                geolocation = GeographicLocation.objects.get_or_create(
-                    geometry=WKTReader().read(n.get_border_wkt(nPoints=n_points)))[0]
+        # Find coverage to set number of points in the geolocation
+        if len(n.vrt.dataset.GetGCPs()) > 0:
+            n.reproject_gcps()
+        geolocation = GeographicLocation.objects.get_or_create(
+            geometry=WKTReader().read(n.get_border_wkt(nPoints=n_points)))[0]
 
-            # create dataset
-            ds, created = Dataset.objects.get_or_create(
-                time_coverage_start=n.get_metadata('time_coverage_start'),
-                time_coverage_end=n.get_metadata('time_coverage_end'),
-                source=source,
-                geographic_location=geolocation,
-                **options)
+        # create dataset
+        ds, created = Dataset.objects.get_or_create(
+            time_coverage_start=n.get_metadata('time_coverage_start'),
+            time_coverage_end=n.get_metadata('time_coverage_end'),
+            source=source,
+            geographic_location=geolocation,
+            **options)
 
         # create parameter
         all_band_meta = n.bands()
@@ -197,7 +158,7 @@ class DatasetManager(models.Manager):
                 params = params.filter(units=units)
             if params.count() >= 1:
                 dsp, dsp_created = DatasetParameter.objects.get_or_create(
-                    dataset=ds, parameter=params[0])
+                        dataset=ds, parameter=params[0])
                 ds.parameters.add(params[0])
 
         # create dataset URI
