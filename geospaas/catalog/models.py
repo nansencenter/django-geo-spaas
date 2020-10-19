7a79541da22aa63c10687287d61c2fdafbbf111d
Adrien Perrin <adrien.perrin@nersc.no>
2020-10-19 16:07:13 +0200

    alter entry_id field from CharField to TextField

--- geospaas/catalog/models.py
+++ geospaas/catalog/models.py
@@ -138,7 +138,7 @@ class Dataset(models.Model):
         )
 
     # DIF required fields
-    entry_id = models.CharField(max_length=80, unique=True, default=uuid.uuid4,
+    entry_id = models.TextField(unique=True, default=uuid.uuid4,
         validators=[
             RegexValidator(r'^[0-9a-zA-Z_.-]*$', 'Only alphanumeric characters are allowed.')
         ]
