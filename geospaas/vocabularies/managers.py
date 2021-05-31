4ee57539c40b2057d7f9c4621bb0021ad33cd8fe
Adrien Perrin <adrien.perrin@nersc.no>
2021-05-31 12:12:23 +0000

    make generic create_from_vocabularies()
    and enable pythesint version support

--- geospaas/vocabularies/managers.py
+++ geospaas/vocabularies/managers.py
@@ -12,13 +12,15 @@
 #-------------------------------------------------------------------------------
 from __future__ import print_function
 
-import os, json, warnings
-import pythesint as pti
 
+import pythesint as pti
 from django.db import models
+
 
 class VocabularyManager(models.Manager):
     """ Base abstract class for all Managers here """
+
+    STANDARD_NAME = 'standard_name'
 
     def update_and_get_list(self, get_list, update, force, version=None):
         """ Get list of Pythesint entires after an update if needed
@@ -64,11 +66,10 @@ class VocabularyManager(models.Manager):
 
     def get_or_create(self, entry, *args, **kwargs):
         """ Get or create database instance from input pythesint entry """
-
-        params = {key : entry[self.mapping[key]] for key in self.mapping}
+        params = {key : entry.get(self.mapping[key], '') for key in self.mapping}
         return super(VocabularyManager, self).get_or_create(**params)
 
-    def create_from_vocabularies(self, force=False, **kwargs):
+    def create_from_vocabularies(self, force=False, versions=None, **kwargs):
         """ Get instances Pythesint and create instances in database.
 
         Parameters
@@ -77,49 +78,55 @@ class VocabularyManager(models.Manager):
             Force update of Vocabulary from Internet ?
 
         """
-        pti_list = self.update_and_get_list(self.get_list, self.update, force)
-        self.create_instances(pti_list)
+        versions = versions if versions else {}
+        pti_lists = (
+            self.update_and_get_list(v['get_list'], v['update'], force, version=versions.get(k))
+            for k, v in self.vocabularies.items()
+        )
+        merged_list = next(pti_lists)  # pti_lists is a generator
+        for list_to_merge in pti_lists:
+            for new_keyword in list_to_merge:
+                standard_name = new_keyword.get(self.STANDARD_NAME)
+                if standard_name:
+                    # check if a keyword with the same standard_name
+                    # already exists in the merged list. If not, add it
+                    add_new_keyword = True
+                    for existing_keyword in merged_list:
+                        if standard_name == existing_keyword[self.STANDARD_NAME]:
+                            add_new_keyword = False
+                            break
+                    if add_new_keyword:
+                        merged_list.append(new_keyword)
+
+        self.create_instances(merged_list)
+
 
 class ParameterManager(VocabularyManager):
-    get_list = pti.get_wkv_variable_list
-    get_list2 = pti.get_cf_standard_name_list
-    update = pti.update_wkv_variable
-    update2 = pti.update_cf_standard_name
+    vocabularies = {
+        'wkv_variable': {
+            'get_list': pti.get_wkv_variable_list,
+            'update': pti.update_wkv_variable
+        },
+        'cf_standard_name': {
+            'get_list': pti.get_cf_standard_name_list,
+            'update': pti.update_cf_standard_name
+        }
+    }
     mapping = dict(standard_name='standard_name',
                    short_name='short_name',
                    units='units')
-
-    def create_from_vocabularies(self, force=False, **kwargs):
-        """ Create parameter instances from the NERSC WKV list and CF-standard names.
-
-        Have to override VocabularyManager.create_from_vocabularies in order to allow
-        merge of CF-variable names and WKVs.
-
-        """
-        warnings.warn(''' Since we do not yet have the mapping between the different
-        vocabularies, the GCMD science keywords are not linked to the catalog
-        parameter table ''')
-        pti_list1 = self.update_and_get_list(self.get_list, self.update, force)
-        pti_list2 = self.update_and_get_list(self.get_list2, self.update2, force)
-        # merge two lists
-        l1_std = [l['standard_name'] for l in pti_list1]
-        for l in pti_list2:
-            if 'standard_name' in l and l['standard_name'] not in pti_list1:
-                pti_list1.append(
-                    dict(
-                        standard_name=l['standard_name'],
-                        short_name='',
-                        units=l['canonical_units']))
-
-        self.create_instances(pti_list1)
 
     def get_by_natural_key(self, standard_name):
         return self.get(standard_name=standard_name)
 
 
 class PlatformManager(VocabularyManager):
-    get_list = pti.get_gcmd_platform_list
-    update = pti.update_gcmd_platform
+    vocabularies = {
+        'gcmd_platform': {
+            'get_list': pti.get_gcmd_platform_list,
+            'update': pti.update_gcmd_platform
+        }
+    }
     mapping = dict(category='Category',
                     series_entity='Series_Entity',
                     short_name='Short_Name',
@@ -128,8 +135,12 @@ class PlatformManager(VocabularyManager):
 
 
 class InstrumentManager(VocabularyManager):
-    get_list = pti.get_gcmd_instrument_list
-    update = pti.update_gcmd_instrument
+    vocabularies = {
+        'gcmd_instrument': {
+            'get_list': pti.get_gcmd_instrument_list,
+            'update': pti.update_gcmd_instrument
+        }
+    }
     mapping = dict(category='Category',
                     instrument_class='Class',
                     type='Type',
@@ -140,8 +151,12 @@ class InstrumentManager(VocabularyManager):
 
 
 class ScienceKeywordManager(VocabularyManager):
-    get_list = pti.get_gcmd_science_keyword_list
-    update = pti.update_gcmd_science_keyword
+    vocabularies = {
+        'gcmd_science_keyword': {
+            'get_list': pti.get_gcmd_science_keyword_list,
+            'update': pti.update_gcmd_science_keyword
+        }
+    }
     mapping = dict(category='Category',
                 topic='Topic',
                 term='Term',
@@ -159,8 +174,12 @@ class ScienceKeywordManager(VocabularyManager):
 
 
 class DataCenterManager(VocabularyManager):
-    get_list = pti.get_gcmd_provider_list
-    update = pti.update_gcmd_provider
+    vocabularies = {
+        'gcmd_provider': {
+            'get_list': pti.get_gcmd_provider_list,
+            'update': pti.update_gcmd_provider
+        }
+    }
     mapping = dict(bucket_level0='Bucket_Level0',
                 bucket_level1='Bucket_Level1',
                 bucket_level2='Bucket_Level2',
@@ -174,8 +193,12 @@ class DataCenterManager(VocabularyManager):
 
 
 class HorizontalDataResolutionManager(VocabularyManager):
-    get_list = pti.get_gcmd_horizontalresolutionrange_list
-    update = pti.update_gcmd_horizontalresolutionrange
+    vocabularies = {
+        'gcmd_horizontalresolutionrange': {
+            'get_list': pti.get_gcmd_horizontalresolutionrange_list,
+            'update': pti.update_gcmd_horizontalresolutionrange
+        }
+    }
     mapping = dict(range='Horizontal_Resolution_Range')
 
     def get_by_natural_key(self, hrr):
@@ -183,8 +206,12 @@ class HorizontalDataResolutionManager(VocabularyManager):
 
 
 class VerticalDataResolutionManager(VocabularyManager):
-    get_list = pti.get_gcmd_verticalresolutionrange_list
-    update = pti.update_gcmd_verticalresolutionrange
+    vocabularies = {
+        'gcmd_verticalresolutionrange': {
+            'get_list': pti.get_gcmd_verticalresolutionrange_list,
+            'update': pti.update_gcmd_verticalresolutionrange
+        }
+    }
     mapping = dict(range='Vertical_Resolution_Range')
 
     def get_by_natural_key(self, vrr):
@@ -192,8 +219,12 @@ class VerticalDataResolutionManager(VocabularyManager):
 
 
 class TemporalDataResolutionManager(VocabularyManager):
-    get_list = pti.get_gcmd_temporalresolutionrange_list
-    update = pti.update_gcmd_temporalresolutionrange
+    vocabularies = {
+        'gcmd_temporalresolutionrange': {
+            'get_list': pti.get_gcmd_temporalresolutionrange_list,
+            'update': pti.update_gcmd_temporalresolutionrange
+        }
+    }
     mapping = dict(range='Temporal_Resolution_Range')
 
     def get_by_natural_key(self, trr):
@@ -201,8 +232,12 @@ class TemporalDataResolutionManager(VocabularyManager):
 
 
 class ProjectManager(VocabularyManager):
-    get_list = pti.get_gcmd_project_list
-    update = pti.update_gcmd_project
+    vocabularies = {
+        'gcmd_project': {
+            'get_list': pti.get_gcmd_project_list,
+            'update': pti.update_gcmd_project
+        }
+    }
     mapping = dict(bucket='Bucket',
                 short_name='Short_Name',
                 long_name='Long_Name')
@@ -212,8 +247,12 @@ class ProjectManager(VocabularyManager):
 
 
 class ISOTopicCategoryManager(VocabularyManager):
-    get_list = pti.get_iso19115_topic_category_list
-    update = pti.update_iso19115_topic_category
+    vocabularies = {
+        'iso19115_topic_category': {
+            'get_list': pti.get_iso19115_topic_category_list,
+            'update': pti.update_iso19115_topic_category
+        }
+    }
     mapping = dict(name='iso_topic_category')
 
     def get_by_natural_key(self, name):
@@ -221,8 +260,12 @@ class ISOTopicCategoryManager(VocabularyManager):
 
 
 class LocationManager(VocabularyManager):
-    get_list = pti.get_gcmd_location_list
-    update = pti.update_gcmd_location
+    vocabularies = {
+        'gcmd_location': {
+            'get_list': pti.get_gcmd_location_list,
+            'update': pti.update_gcmd_location
+        }
+    }
     mapping = dict(category='Location_Category',
                 type='Location_Type',
                 subregion1='Location_Subregion1',
