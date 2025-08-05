82d5eae5399433cece95617e886c3208df6bb013
Adrien Perrin <adrien.perrin@nersc.no>
2025-08-05 09:40:30 +0000

    initialize vocabularies UI

--- geospaas/vocabularies/urls.py
+++ geospaas/vocabularies/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
 
-
 import geospaas.vocabularies.views as views
+
 
 app_name = 'vocabularies'
 
@@ -11,5 +11,6 @@ router.register(r'keywords', views.KeywordViewSet)
 router.register(r'parameters', views.ParameterViewSet)
 
 urlpatterns = [
+    path('', views.VocabulariesView.as_view()),
     path('api/', include((router.urls, 'api'))),
 ]
--- geospaas/vocabularies/views.py
+++ geospaas/vocabularies/views.py
@@ -4,6 +4,13 @@ from rest_framework.viewsets import ModelViewSet
 import geospaas.vocabularies.models as models
 import geospaas.vocabularies.serializers as serializers
 import geospaas.vocabularies.filters as filters
+from geospaas.base_viewer.views import GeoSPaaSView
+
+
+class VocabulariesView(GeoSPaaSView):
+    """"""
+    template_name = 'vocabularies/vocabularies.html'
+    tab_label = 'Vocabularies'
 
 
 class KeywordViewSet(ModelViewSet):
